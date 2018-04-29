2a75bdca8462dd2c954a9246ddc1ccd1886305e2
cody <codyjeromegreen@gmail.com>
2018-04-29 09:52:48 -0700

    finished 28 moving on to 29

--- lab-cody/src/components/edit-note.jsx
+++ lab-cody/src/components/edit-note.jsx
@@ -3,23 +3,32 @@ import React from 'react';
 class Edit extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        title: this.props.title,
+        content: this.props.content,
+        id: this.props.id
+    }
+    this.update = this.update.bind(this);
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
   }
 
   update(ev) {
+      let newState = {};
+      newState[ev.target.value] = ev.target.value;
+      this.setState(newState);
 
   }
 
   save(ev) {
-
+    event.preventDefault();
+    this.props.saveUpdate(this.save)
   }
 
   cancel(ev) {
-      
+    event.preventDefault();
+    this.props.cancelUpdate();
   }
-
-
-
-
 
 
   render() {
